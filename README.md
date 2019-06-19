5d77e6f9a4ce1b7a00ae5f08c3dfaeb5c6818752
chanduse <tatc250@gmail.com>
2019-06-19 17:46:13 +0530

    upload

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# udacity-Arcade-game
+# Classic Arcade Game Clone Project
+
+## Table of Contents
+
+-   [Instructions](#instructions)
+-   [Contributing](#contributing)
+
+## Instructions
+
+Use this [rubric](https://review.udacity.com/#!/rubrics/15/view) for self-checking your submission.
+
+Make sure the functions you write are **object-oriented** - either class functions (like `Player` and `Enemy`) or class prototype functions such as `Enemy.prototype.checkCollisions`. Also make sure that the keyword `this` is used appropriately within your class and class prototype functions to refer to the object the function is called upon.
+
+Your **README.md** file should be updated with instructions on both how to 1. Run and 2. Play your arcade game.
+
+For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+
+## Contributing
+
+This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
+
+### Clone or download file given by Udacity
+
+-   Read all the information and coding engine provided by Udacity
+-   Clone or download zip file
+-   After that extracting the zip file , I observed file structure a(`Arcade game`, `CSS/style.css`,
+                                                                    `img/char-boy.png`, and on soon....)
+
+-   I follow the below steps to accomplished the Project :
+
+### In Html file
+
+-   Adding the icon using link tag and also title
+-   Title of the page **Arcade game**
+    ### In  app.js file
+-   initial calling x,y,move and sprite using **this keyword**
+-   Enemy is randomly coming in canvas when using mathematical calculation using random functions
+-   After that player are placed in canvas and moves are done using moves variable
+-   In enemy position `x > 500` then it is multiply with 520 \* random function
+     enemy position 'x > 0' then it is multiply with 30 and 100 == random function
+-   After that char-horn-girl.png is moving in canvas
+-   Implemented the below functions for Player.
+     1.render()
+     2.update() and
+     3.handleInput().
+
+    1.  render() is for placing a player sprite on the canvas.
+    2.  update() is used for updating the position of the player
+    3.  handleInput() is used handle the left-arrow , right-arrow and up and down arrow are handling
+
+-   After All function are working score are displayed in top corner in the canvas
+-   If girl are touch enemy then score will appear Zero
---
+++ js/app.js
@@ -0,0 +1,142 @@
+var count = 0;
+var topScore = 0;
+// var score = document.getElementById("count");
+// Enemies our player must avoid
+var Enemy = function(x, y, move) {
+  // Variables applied to each of our instances go here,
+  // we've provided one for you to get started
+  this.x = x;
+  this.y = y;
+  this.move = move;
+  // The image/sprite for our enemies, this uses
+  // a helper we've provided to easily load images
+  this.sprite = 'images/lorry.png';
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+  this.x = this.x + this.move * dt;
+  if (this.x > 500) {
+    this.move = 100 + Math.floor(Math.random() * 520);
+    this.x = 0;
+  }
+  if (this.x == 0) {
+    this.move = 100 + Math.floor(Math.random() * 30);
+  }
+
+  if (this.x == 0) {
+    this.move = 100 + Math.floor(Math.random() * 100);
+  }
+  // player moves by char-boy and count are adding into count
+  if (player.x < this.x + 80 && player.x + 80 > this.x && player.y < this.y + 80 && player.y + 80 > this.y) {
+    if (topScore < count) {
+      topScore = count;
+      ctx.fillText("highScore : " + topScore, 400, 20);
+    }
+    count = 0;
+    topScore = count;
+    player.x = 200;
+    player.y = 400;
+    player.move = 'images/char-boy.png';
+  }
+
+  // You should multiply any movement by the dt parameter
+  // which will ensure the game runs at the same speed for
+  // all computers.
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  drawScore();
+};
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+class Player {
+  constructor(x, y, sprite) {
+    this.x = x;
+    this.y = y;
+    this.sprite = 'images/char-horn-girl.png';
+  }
+}
+Player.prototype.update = function(dt) {}
+// resources for images palced for image in canvas
+Player.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+var allEnemies = [];
+var enemyPostion = [60, 139, 223];
+enemyPostion.map((item) => {
+  var enemyItem = new Enemy(0, item, 180);
+  console.log(enemyItem);
+  allEnemies.push(enemyItem);
+})
+// player goes does down, up, sides properties are written their
+// players are consequently done in canvas
+var player = new Player(200, 400);
+Player.prototype.handleInput = function(key) {
+  switch (key) {
+    case 'left':
+      this.x = this.x - 101;
+      if (this.x < 0) {
+        this.x = 0;
+      }
+      break;
+    case 'right':
+      this.x = this.x + 101;
+      if (this.x > 400) {
+        this.x = 404;
+      }
+      break;
+    case 'down':
+      this.y = this.y + 83;
+      if (this.y > 400) {
+        this.y = 400;
+      }
+      break;
+    case 'up':
+      this.y = this.y - 83;
+      if (this.y < 0) {
+        this.y = -14;
+        setTimeout(() => {
+          count = count + 1;
+          this.x = 200;
+          this.y = 400;
+        }, 50);
+      }
+      break;
+    default:
+  }
+
+}
+
+// score and highScore display on the screen using ctx "canvas"
+function drawScore() {
+  ctx.font = "16px bold";
+  ctx.fillText("Score : " + count, 8, 20);
+  // ctx.fillText("highScore : " + topScore, 400, 20);
+}
+
+if (topScore < count) {
+  count = topScore;
+}
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
+
+  player.handleInput(allowedKeys[e.keyCode]);
+});
